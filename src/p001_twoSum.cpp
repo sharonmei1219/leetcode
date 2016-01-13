3607938e9c3eac8b9310290767f9a4caacc77d98
xuelan.mei <sharonmei1219@gmail.com>
2016-01-13 08:07:25 +0800

    problem 001 two sum accepted

---
+++ src/p001_twoSum.cpp
@@ -0,0 +1,66 @@
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include "gtest/gtest.h"
+using namespace std;
+
+class IndexCompare{
+public:
+    IndexCompare(vector<int> & array):array(array){}
+    const vector<int>  & array;
+    bool operator()(int a, int b) const {return array[a] < array[b];}
+};
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> index;
+        for(size_t i = 0; i < nums.size(); i++)
+            index.push_back(i);
+        sort(index.begin(), index.end(), IndexCompare(nums));
+
+
+        int front = 0, back = index.size() - 1;
+        int sum = nums[index[front]] + nums[index[back]];
+        while (front != back && sum != target){
+            if (sum > target){
+                back = back - 1;
+                sum = nums[index[front]] + nums[index[back]];
+            }
+            else if(sum < target){
+                front = front + 1;
+                sum = nums[index[front]] + nums[index[back]];
+            }
+        }
+
+        vector<int> result;
+        if(index[front] > index[back]){
+            result.push_back(index[back] + 1);
+            result.push_back(index[front] + 1);
+        }else{
+            result.push_back(index[front] + 1);
+            result.push_back(index[back] + 1);
+        }
+        return result;
+    }
+};
+
+class TestTwoSum : public ::testing::Test{
+};
+
+TEST_F(TestTwoSum, sortedIndex){
+	vector<int> nums;
+	vector<int> index;
+	nums.push_back(2);
+	nums.push_back(3);
+	nums.push_back(4);
+
+
+	for(size_t i = 0; i < nums.size(); i++)
+		index.push_back(i);
+	sort(index.begin(), index.end(), IndexCompare(nums));
+
+	EXPECT_EQ(0, index[0]);
+	EXPECT_EQ(1, index[1]);
+	EXPECT_EQ(2, index[2]);
+};
