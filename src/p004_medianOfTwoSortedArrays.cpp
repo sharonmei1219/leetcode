18c5a193c77ca9fcfdd8f50adaf7665207dc5fc6
xuelan.mei <sharonmei1219@gmail.com>
2016-01-19 14:46:15 +0800

    problem 004 solved in log(n+m)

--- src/p004_medianOfTwoSortedArrays.cpp
+++ src/p004_medianOfTwoSortedArrays.cpp
@@ -3,6 +3,31 @@ using namespace std;
 #include "gtest/gtest.h"
 
 
+class Solution{
+public:
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2){
+        int l = (nums1.size() + nums2.size() -1)/2;
+        int r = (nums1.size() + nums2.size())/2;
+        return (find(nums1, 0, nums2, 0, l) + find(nums1, 0, nums2, 0, r))/2;
+    }
+
+    double find(vector<int>& nums1, int start_1, vector<int>& nums2, int start_2, int kth){
+        if(start_1 >= nums1.size()) return nums2[start_2 + kth];
+        if(start_2 >= nums2.size()) return nums1[start_1 + kth];
+        if(kth == 0) return min(nums1[start_1], nums2[start_2]);
+
+        int mid1 = 0xEFFFFFF, mid2 = 0xEFFFFFF;
+        if(kth/2 + start_1 < nums1.size()) mid1 = nums1[kth/2 + start_1];
+        if(kth/2 + start_2 < nums2.size()) mid2 = nums2[kth/2 + start_2];
+        if(mid1 < mid2) return find(nums1, (kth+1)/2 + start_1, nums2, start_2, kth/2);
+        else return find(nums1, start_1, nums2, (kth+1)/2 + start_2, kth/2);
+    }
+};
+
+
+
+
+//------ revised time O(m+n), space O(1)
 class Solution1{
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2){
@@ -40,7 +65,7 @@ public:
 };
 
 
-
+//------- time: O(m+n) space: O(m+n) -----
 
 class Solution2 {
 public:
@@ -89,10 +114,62 @@ TEST_F(Test004, merge2Vectors){
 	nums2.push_back(2);
 	nums2.push_back(4);
 
-	Solution * solution = new Solution();
+	Solution2 * solution = new Solution2();
 	vector<int> result = solution->merge(nums1, nums2);
 	EXPECT_EQ(1, result[0]);
 	EXPECT_EQ(2, result[1]);
 	EXPECT_EQ(3, result[2]);
 	EXPECT_EQ(4, result[3]);
+};
+
+TEST_F(Test004, solutionOfDivide){
+    vector<int> nums1;
+    vector<int> nums2;
+
+    nums1.push_back(1);
+
+    nums2.push_back(1);
+
+    Solution * solution = new Solution();
+    EXPECT_EQ(1, solution->find(nums1, 0, nums2, 0, 0));
+    EXPECT_EQ(1, solution->find(nums1, 0, nums2, 0, 1));
+    EXPECT_EQ(1, solution->findMedianSortedArrays(nums1, nums2));
+};
+
+TEST_F(Test004, solutionOfDivide11And12){
+    vector<int> nums1;
+    vector<int> nums2;
+
+    nums1.push_back(1);
+    nums1.push_back(1);
+
+    nums2.push_back(1);
+    nums2.push_back(2);
+
+    Solution * solution = new Solution();
+    EXPECT_EQ(1, (nums1.size() + nums2.size() -1)/2);
+    EXPECT_EQ(2, (nums1.size() + nums2.size())/2);
+
+    EXPECT_EQ(1, solution->find(nums1, 0, nums2, 0, 1));
+    EXPECT_EQ(1, solution->find(nums1, 0, nums2, 0, 2));
+    EXPECT_EQ(1, solution->findMedianSortedArrays(nums1, nums2));
+};
+
+TEST_F(Test004, solutionOfDivide12And12){
+    vector<int> nums1;
+    vector<int> nums2;
+
+    nums1.push_back(1);
+    nums1.push_back(2);
+
+    nums2.push_back(1);
+    nums2.push_back(2);
+
+    Solution * solution = new Solution();
+    EXPECT_EQ(1, (nums1.size() + nums2.size() -1)/2);
+    EXPECT_EQ(2, (nums1.size() + nums2.size())/2);
+
+    EXPECT_EQ(1, solution->find(nums1, 0, nums2, 0, 1));
+    EXPECT_EQ(2, solution->find(nums1, 0, nums2, 0, 2));
+    EXPECT_EQ(1.5, solution->findMedianSortedArrays(nums1, nums2));
 };
